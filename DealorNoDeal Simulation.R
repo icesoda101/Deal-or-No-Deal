64a58142701e0ab0e7f2097de862e3321dd79ed2
icesoda101 <122189915+icesoda101@users.noreply.github.com>
2023-01-08 23:53:28 -0500

    quick up3

--- DealorNoDeal Simulation.R
+++ DealorNoDeal Simulation.R
@@ -1,5 +1,4 @@
 f <- function() {
-  ##a02q5b
   s <- c(.01, 1,5, 10, 25, 50, 75, 100 ,200, 300, 400, 500 ,750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000 ,750000, 1000000)
   contestant_case = sample(s,1,replace=FALSE)
   remain1 = setdiff(s,contestant_case)
